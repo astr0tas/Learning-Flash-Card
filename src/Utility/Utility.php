849c8515b5e499395aabd1923dcacb80be38aae1
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2026-02-02 15:40:41 +0700

    Update Utility.php

--- src/Utility/Utility.php
+++ src/Utility/Utility.php
@@ -6,11 +6,21 @@ use App\Config\Constants;
 
 class Utility
 {
+  /**
+   * Generate a random token string
+   * @param int $length Length of the random bytes before hex encoding
+   * @return string Hexadecimal representation of the random token, twice the length of input bytes
+   */
   public static function generateRandomToken(int $length = 32): string
   {
     return bin2hex(random_bytes($length));
   }
 
+  /**
+   * Hash a given string using Bcrypt algorithm
+   * @param string $input The input string to hash
+   * @return string The Bcrypt hashed string
+   */
   public static function hashString(string $input): string
   {
     return password_hash($input, PASSWORD_BCRYPT, [
@@ -18,6 +28,11 @@ class Utility
     ]);
   }
 
+  /**
+   * Parse the "state" parameter from Google Sign-In redirect
+   * @param string $state The state string to parse
+   * @return array Associative array of parsed state parameters
+   */
   public static function parseGSIState(string $state): array
   {
     $result = [];
