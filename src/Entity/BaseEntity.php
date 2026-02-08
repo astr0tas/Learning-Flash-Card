a4447d8ff1456a4248cad1dc80b4cf779a310432
NghiaDuong <nghia369ty@gmail.com>
2026-02-08 11:39:25 +0700

    Update BaseEntity.php

--- src/Entity/BaseEntity.php
+++ src/Entity/BaseEntity.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\MappedSuperclass]
 #[ORM\HasLifecycleCallbacks] // Vital for triggering PrePersist/PreUpdate
-class BaseEntity
+abstract class BaseEntity
 {
   #[ORM\Id]
   #[ORM\GeneratedValue]
