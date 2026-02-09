c96b1ebcce70260320bfc99cdb57c71923ef6715
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2026-02-09 14:32:31 +0700

    fix: include `entityManager` to `BaseRepository`

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -3,5 +3,16 @@
 namespace App\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
-abstract class BaseRepository extends ServiceEntityRepository {}
+abstract class BaseRepository extends ServiceEntityRepository
+{
+  protected $entityManager;
+
+  public function __construct(ManagerRegistry $registry, string $entityClass)
+  {
+    parent::__construct($registry, $entityClass);
+
+    $this->entityManager = $this->getEntityManager();
+  }
+}
