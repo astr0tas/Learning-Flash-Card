99a6fbd573b3e63d15190b1009bc89d7b6f9d5c8
NghiaDuong <101704565+astr0tas@users.noreply.github.com>
2026-03-23 14:35:30 +0700

    feature: add dropdown component and make preparations for modals

--- assets/scripts/dropdown.js
+++ assets/scripts/dropdown.js
@@ -3,8 +3,6 @@
 export const dropdownProcessor = {
   dropdowns: {
     appMenuDropdown: false,
-    addDropdown: false,
-    optionDropdown: false,
   },
   checkAppClick: function (event)
   {
@@ -12,18 +10,6 @@ export const dropdownProcessor = {
     const menu = this.$refs.appMenuDropdownRef;
     if (menu && !menu.contains(event.target)) {
         this.dropdowns.appMenuDropdown = false;
-    }
-
-    // Check if add dropdown exists AND click was outside
-    const addDropdownElem = this.$refs.addDropdownRef;
-    if (addDropdownElem && !addDropdownElem.contains(event.target)) {
-        this.dropdowns.addDropdown = false;
-    }
-
-    // Check if option dropdown exists AND click was outside
-    const optionDropdownElem = this.$refs.optionDropdownRef;
-    if (optionDropdownElem && !optionDropdownElem.contains(event.target)) {
-        this.dropdowns.optionDropdown = false;
     }
   }
 };
