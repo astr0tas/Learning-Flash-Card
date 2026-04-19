3cfdcbbdc432fe7db5d5719ed608e858efddf9be
NghiaDuongUbuntu <nghia369ty@gmail.com>
2026-04-20 00:39:45 +0700

    update: making preps for card component

--- src/Twig/Components/Card.php
+++ src/Twig/Components/Card.php
@@ -3,8 +3,16 @@
 namespace App\Twig\Components;
 
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use App\Config\Constants;
 
 #[AsTwigComponent]
 final class Card
 {
+  public int $cardId;
+  public string $cardTitle;
+  public ?string $cardSubTitle = null;
+  public ?string $cardDescription = null;
+  public ?string $model = null;
+  public string $cardColor = Constants::FLASH_CARD_DEFAULT_COLOR;
+  public string $cardTextColor = Constants::FLASH_CARD_DEFAULT_TEXT_COLOR;
 }
