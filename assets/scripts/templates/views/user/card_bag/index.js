07e2613f4322cf5a4fc83b8b1f2b964213874c55
NghiaDuongUbuntu <nghia369ty@gmail.com>
2026-04-20 00:39:14 +0700

    feature:  process list of bags and cards return from the controller and store it in  js data

--- assets/scripts/templates/views/user/card_bag/index.js
+++ assets/scripts/templates/views/user/card_bag/index.js
@@ -2,6 +2,8 @@ document.addEventListener('alpine:init', () =>
 {
   Alpine.data('cardBag', () => ({
     normalInputClass: "w-full rounded-lg px-3.5 py-3 outline-none focus:ring-2 focus:ring-offset-0 transition-all peer select-none border border-gray-400 focus:ring-blue-200 focus:ring-offset-white focus:border-blue-500 !text-base",
+    bagList,
+    cardList,
     selectedBags: [],
     selectedCards: [],
     openNewBagModal()
