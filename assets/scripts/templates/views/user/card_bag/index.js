9e773a203fc6abd5b4cf5b582e1cda1bc2f45121
NghiaDuongUbuntu <nghia369ty@gmail.com>
2026-04-30 13:51:56 +0700

    minor updates

--- assets/scripts/templates/views/user/card_bag/index.js
+++ assets/scripts/templates/views/user/card_bag/index.js
@@ -2,11 +2,8 @@ document.addEventListener('alpine:init', () =>
 {
   Alpine.data('cardBag', () => ({
     normalInputClass: "w-full rounded-lg px-3.5 py-3 outline-none focus:ring-2 focus:ring-offset-0 transition-all peer select-none border border-gray-400 focus:ring-blue-200 focus:ring-offset-white focus:border-blue-500 !text-base",
-    bagList,
     filteredBagList: JSON.parse(JSON.stringify(bagList)),
-    cardList,
     filteredCardList: JSON.parse(JSON.stringify(cardList)),
-    viewCardList,
     viewFilteredCardList: JSON.parse(JSON.stringify(viewCardList)),
     selectCard: '',
     selectedBags: [],
@@ -17,7 +14,7 @@ document.addEventListener('alpine:init', () =>
     },
     openCardDetailModal(index)
     {
-      this.$dispatch('set-selected-card-data', this.viewCardList[index]);
+      this.$dispatch('set-selected-card-data', viewCardList[index]);
       document.getElementById('card_detail_modal').setAttribute('open',true);
     },
     closeCardDetailModal()
@@ -33,26 +30,26 @@ document.addEventListener('alpine:init', () =>
     {
       if (!search)
       {
-        this.filteredBagList = JSON.parse(JSON.stringify(this.bagList));
-        this.filteredCardList = JSON.parse(JSON.stringify(this.cardList));
-        this.viewFilteredCardList = JSON.parse(JSON.stringify(this.viewCardList));
+        this.filteredBagList = JSON.parse(JSON.stringify(bagList));
+        this.filteredCardList = JSON.parse(JSON.stringify(cardList));
+        this.viewFilteredCardList = JSON.parse(JSON.stringify(viewCardList));
         return;
       }
 
       const normalizedSearch = this.removeDiacritics(search.toLowerCase());
       const searchKeywords = normalizedSearch.split(/[~`!@#$%^&*()_+\-\=\[\]{}\\|;':"<>,./? ]+/).filter(keyword => keyword);
 
-      this.filteredBagList = this.bagList.filter(bag => {
+      this.filteredBagList = bagList.filter(bag => {
         const normalizedBagName = this.removeDiacritics(bag.name.toLowerCase());
         return searchKeywords.some(keyword => normalizedBagName.includes(keyword));
       });
 
-      this.filteredCardList = this.cardList.filter(card => {
+      this.filteredCardList = cardList.filter(card => {
         const normalizedCardTitle = this.removeDiacritics(card.title.toLowerCase());
         return searchKeywords.some(keyword => normalizedCardTitle.includes(keyword));
       });
 
-      this.viewFilteredCardList = this.viewCardList.filter(card => {
+      this.viewFilteredCardList = viewCardList.filter(card => {
         const normalizedCardTitle = this.removeDiacritics(card.title.toLowerCase());
         return searchKeywords.some(keyword => normalizedCardTitle.includes(keyword));
       });
