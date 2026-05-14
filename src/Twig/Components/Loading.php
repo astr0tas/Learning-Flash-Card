76be0495d002dfa2d1c73291beac5511b7924a97
NghiaDuong <nghia369ty@gmail.com>
2026-05-14 14:48:58 +0000

    fix: add property for removing loading component's backdrop

--- src/Twig/Components/Loading.php
+++ src/Twig/Components/Loading.php
@@ -5,4 +5,6 @@ namespace App\Twig\Components;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
-final class Loading {}
+final class Loading {
+  public bool $displayBackdrop = true;
+}
